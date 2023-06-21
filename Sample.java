db1733edae073a608596e40884cd3b6916fbc0c8
Shravanrm123 <shravanrm333@gmail.com>
2023-06-21 13:05:06 +0530

    a value change

--- Sample.java
+++ Sample.java
@@ -1,10 +1,7 @@
 class GitFirst{
     public static void main(String args[])
     {
-<<<<<<< HEAD
         System.out.println("a is 55");
-=======
-    
->>>>>>> 601a75acb7b1550be5b089fa88edd69abe5e8de4
+
     }
 }
